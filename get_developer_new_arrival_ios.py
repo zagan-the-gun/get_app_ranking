ed9f628f677964c24a04c7160371256683e91e91
zagan <zagan.the.gun@gmail.com>
2019-09-30 20:16:52 +0900

    不要なコメントアウト削除

--- get_developer_new_arrival_ios.py
+++ get_developer_new_arrival_ios.py
@@ -14,7 +14,7 @@ args = sys.argv
 
 DATABASE_URL='postgresql://'+ args[1] + ':' + args[2] + '@'+ args[3] + ':5439/'+ args[4]
 LOG='/tmp/superset.log'
-DEBUG=True
+DEBUG=False
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
@@ -24,24 +24,19 @@ with get_connection() as conn:
     with conn.cursor() as cur:
         cur.execute("SELECT DISTINCT publisher_id, publisher_name FROM superset_schema.app_details WHERE platform = 2 ORDER BY publisher_id ;")
         for dev in cur:
-            print(dev)
             dev_id=dev[1].replace(" ", "-").replace(".","").replace(",","").replace(":","").replace(";",'').lower()
-            print(dev_id)
 
             # iTunesからページ取得
             target_url = "https://itunes.apple.com/search?entity=software&country=jp&lang=ja_jp&limit=200&term={dev_id}".format(dev_id=dev_id)
-            print(target_url)
             response = requests.get(target_url, timeout=(10.0, 10.0)) #requestsを使って、webから取得
             # リトライ
             if response.status_code != requests.codes.ok:
                 response = requests.get(target_url, timeout=(10.0, 10.0)) #requestsを使って、webから取得
-                print(response)
             app_dict = json.loads(response.text)
 
             # 個数チェック
             if app_dict['resultCount'] == 0:
                 target_url = "https://itunes.apple.com/search?entity=software&country=us&lang=ja_jp&limit=200&term={dev_id}".format(dev_id=dev_id)
-                print(target_url)
                 response = requests.get(target_url, timeout=(10.0, 10.0)) #requestsを使って、webから取得
                 # リトライ
                 if response.status_code != requests.codes.ok:
@@ -52,23 +47,22 @@ with get_connection() as conn:
             # 個数チェック
             if app_dict['resultCount'] == 0:
                 target_url = "https://itunes.apple.com/search?entity=software&country=cn&lang=ja_jp&limit=200&term={dev_id}".format(dev_id=dev_id)
-                print(target_url)
                 response = requests.get(target_url, timeout=(10.0, 10.0)) #requestsを使って、webから取得
                 # リトライ
                 if response.status_code != requests.codes.ok:
                     response = requests.get(target_url, timeout=(10.0, 10.0)) #requestsを使って、webから取得
-                    print(response)
                 app_dict = json.loads(response.text)
  
-#            print(response.text)
-#            print(app_dict)
-#            print("DEBUG DEBUG DEBUG")
-
             for app in app_dict['results']:
-                print(app['trackId'])
-                with get_connection() as conn:
-                    with conn.cursor() as cur:
-                        cur.execute("INSERT INTO superset_schema.app_ids(app_id, platform, created_at) SELECT '" + str(app['trackId']) + "', 2, CONVERT_TIMEZONE('JST', SYSDATE) WHERE NOT EXISTS ( SELECT app_id FROM superset_schema.app_ids WHERE app_id = '" + str(app['trackId']) + "'); ")
+                if app['primaryGenreId'] == 6014:
+                    if DEBUG:
+                        print(app['trackName'])
+                        print(app['sellerName'])
+                        print(app['trackViewUrl'])
+
+                    with get_connection() as conn:
+                        with conn.cursor() as cur:
+                            cur.execute("INSERT INTO superset_schema.app_ids(app_id, platform, created_at) SELECT '" + str(app['trackId']) + "', 2, CONVERT_TIMEZONE('JST', SYSDATE) WHERE NOT EXISTS ( SELECT app_id FROM superset_schema.app_ids WHERE app_id = '" + str(app['trackId']) + "'); ")
 
 with open(LOG, mode='a') as f:
     f.write(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+": get_developer_new_arrival_ios\n")
