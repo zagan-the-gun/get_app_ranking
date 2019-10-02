f61bc45abe1d0bcfb47fb116672e402fb6b5a2ba
zagan <zagan.the.gun@gmail.com>
2019-10-02 15:24:38 +0900

    削除

--- check_ios.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-source /home/ishizuka/get_app_ranking/env3.7/bin/activate
-/home/ishizuka/get_app_ranking/check_ios.py superset_user 1Qazxsw2 superset.cl2l1vcubsxz.ap-northeast-1.redshift.amazonaws.com superset T1JMA2W2C/BNM7HRETY/Y3zclM24KPbqNEmgnaZNkEcI
