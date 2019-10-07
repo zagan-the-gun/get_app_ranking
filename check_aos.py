00227487c8dbc8e8396eadb19ac1bbc07ccea40c
zagan <zagan.the.gun@gmail.com>
2019-10-07 18:02:02 +0900

    SLACKへの投稿形式を変更

--- check_aos.py
+++ check_aos.py
@@ -60,12 +60,16 @@ with get_connection() as conn:
                     app = get_dict_resultset("SELECT * FROM superset_schema.app_details WHERE app_id = '{}'".format(app_id[0]))
 
                     SCREENSHOTS=""
+                    SCREENSHOTS2=""
                     if app[0]['screenshots'] is not None:
                         for i,s in enumerate(json.loads(app[0]['screenshots'])):
-                            SCREENSHOTS=SCREENSHOTS + "<" + s + "|" + str(i) + "> "
+                            if i < 3:
+                                SCREENSHOTS=SCREENSHOTS + "<" + s + "|" + str(i) + "> "
+                            else:
+                                SCREENSHOTS2=SCREENSHOTS2 + "<" + s + "|" + str(i) + "> "
 
-                    text="{icon_url}\n<https://play.google.com/store/apps/details?id={app_id} | {app_name}> の android アプリが非公開になりました\nジャンル: {genre}\nコンテンツレーティング: {content_rating}\nデベロッパー: <https://play.google.com/store/apps/developer?id={publisher_id} | {publisher_name}>\nインストール数: {installs}\nスクリーンショット: {screenshots}\nビデオ: {video}\n"\
-                            .format(icon_url=app[0]['icon_url'], app_id=app[0]['app_id'], app_name=app[0]['app_name'], genre=app[0]['genre'], content_rating=app[0]['content_rating'], publisher_id=app[0]['publisher_id'], publisher_name=app[0]['publisher_name'], rating=app[0]['rating'], rating_count=app[0]['rating_count'], reviews=app[0]['reviews'], description=app[0]['description'], screenshots=SCREENSHOTS, video=app[0]['video'], installs=app[0]['installs'])
+                    text="{icon_url}\n<https://play.google.com/store/apps/details?id={app_id} | {app_name}> の android アプリが非公開になりました\nジャンル: {genre}\nコンテンツレーティング: {content_rating}\nデベロッパー: <https://play.google.com/store/apps/developer?id={publisher_id} | {publisher_name}>\nインストール数: {installs}\nビデオ: {video}\n"\
+                            .format(icon_url=app[0]['icon_url'], app_id=app[0]['app_id'], app_name=app[0]['app_name'], genre=app[0]['genre'], content_rating=app[0]['content_rating'], publisher_id=app[0]['publisher_id'], publisher_name=app[0]['publisher_name'], rating=app[0]['rating'], rating_count=app[0]['rating_count'], reviews=app[0]['reviews'], description=app[0]['description'], video=app[0]['video'], installs=app[0]['installs'])
 
                     # Slackに死亡を報告
                     requests.post("https://hooks.slack.com/services/" + SLACK_URL, data = json.dumps({
@@ -74,6 +78,21 @@ with get_connection() as conn:
                         'icon_emoji': u':smile_cat:',  #アイコン
                         'link_names': 1,  #名前をリンク化
                     }))
+                    # スクリーンショット掲載
+                    if SCREENSHOTS != "":
+                        requests.post("https://hooks.slack.com/services/" + SLACK_URL, data = json.dumps({
+                            'text': "スクリーンショット: " + SCREENSHOTS,  #通知内容
+                            'username': u'確認',  #ユーザー名
+                            'icon_emoji': u':smile_cat:',  #アイコン
+                            'link_names': 1,  #名前をリンク化
+                        }))
+                    if SCREENSHOTS2 != "":
+                        requests.post("https://hooks.slack.com/services/" + SLACK_URL, data = json.dumps({
+                            'text': SCREENSHOTS2,  #通知内容
+                            'username': u'確認',  #ユーザー名
+                            'icon_emoji': u':smile_cat:',  #アイコン
+                            'link_names': 1,  #名前をリンク化
+                        }))
 
                     if DEBUG:
                         print("app is dead. {}".format(app_id[0]))
@@ -102,12 +121,16 @@ with get_connection() as conn:
                     REVIVAL_DATE=(app[0]['updated_at'] - datetime.datetime.now(tz=jst)).days
 
                     SCREENSHOTS=""
+                    SCREENSHOTS2=""
                     if app[0]['screenshots'] is not None:
                         for i,s in enumerate(json.loads(app[0]['screenshots'])):
-                            SCREENSHOTS=SCREENSHOTS + "<" + s + "|" + str(i) + "> "
+                            if i < 3:
+                                SCREENSHOTS=SCREENSHOTS + "<" + s + "|" + str(i) + "> "
+                            else:
+                                SCREENSHOTS2=SCREENSHOTS2 + "<" + s + "|" + str(i) + "> "
 
-                    text="{icon_url}\n<https://play.google.com/store/apps/details?id={app_id} | {app_name}> の android アプリが再公開になりました\n{revival_date} 日ぶり\nジャンル: {genre}\nコンテンツレーティング: {content_rating}\nデベロッパー: <https://play.google.com/store/apps/developer?id={publisher_id} | {publisher_name}>\nインストール数: {installs}\nスクリーンショット: {screenshots}\nビデオ: {video}\n"\
-                            .format(icon_url=app[0]['icon_url'], app_id=app[0]['app_id'], app_name=app[0]['app_name'], revival_date=REVIVAL_DATE, genre=app[0]['genre'], content_rating=app[0]['content_rating'], publisher_id=app[0]['publisher_id'], publisher_name=app[0]['publisher_name'], rating=app[0]['rating'], rating_count=app[0]['rating_count'], reviews=app[0]['reviews'], description=app[0]['description'], screenshots=SCREENSHOTS, video=app[0]['video'], installs=app[0]['installs'])
+                    text="{icon_url}\n<https://play.google.com/store/apps/details?id={app_id} | {app_name}> の android アプリが再公開になりました\n{revival_date} 日ぶり\nジャンル: {genre}\nコンテンツレーティング: {content_rating}\nデベロッパー: <https://play.google.com/store/apps/developer?id={publisher_id} | {publisher_name}>\nインストール数: {installs}\nビデオ: {video}\n"\
+                            .format(icon_url=app[0]['icon_url'], app_id=app[0]['app_id'], app_name=app[0]['app_name'], revival_date=REVIVAL_DATE, genre=app[0]['genre'], content_rating=app[0]['content_rating'], publisher_id=app[0]['publisher_id'], publisher_name=app[0]['publisher_name'], rating=app[0]['rating'], rating_count=app[0]['rating_count'], reviews=app[0]['reviews'], description=app[0]['description'], video=app[0]['video'], installs=app[0]['installs'])
 
                     # Slackに生存を報告
                     requests.post("https://hooks.slack.com/services/" + SLACK_URL, data = json.dumps({
@@ -116,6 +139,21 @@ with get_connection() as conn:
                         'icon_emoji': u':smile_cat:',  #アイコン
                         'link_names': 1,  #名前をリンク化
                     }))
+                    # スクリーンショット掲載
+                    if SCREENSHOTS != "":
+                        requests.post("https://hooks.slack.com/services/" + SLACK_URL, data = json.dumps({
+                            'text': "スクリーンショット: " + SCREENSHOTS,  #通知内容
+                            'username': u'確認',  #ユーザー名
+                            'icon_emoji': u':smile_cat:',  #アイコン
+                            'link_names': 1,  #名前をリンク化
+                        }))
+                    if SCREENSHOTS2 != "":
+                        requests.post("https://hooks.slack.com/services/" + SLACK_URL, data = json.dumps({
+                            'text': SCREENSHOTS2,  #通知内容
+                            'username': u'確認',  #ユーザー名
+                            'icon_emoji': u':smile_cat:',  #アイコン
+                            'link_names': 1,  #名前をリンク化
+                        }))
 
                     if DEBUG:
                         print("app is alive. {}".format(app_id[0]))
