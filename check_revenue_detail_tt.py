51da7681adb2032666c85e4522e8a3ab5cbad173
zagan <zagan.the.gun@gmail.com>
2019-10-30 18:05:17 +0900

    高速化

--- check_revenue_detail_tt.py
+++ check_revenue_detail_tt.py
@@ -2,13 +2,10 @@
 # -*- coding: utf8 -*-
 
 import requests
-import json
-from bs4 import BeautifulSoup
 import os
 import psycopg2
 import psycopg2.extras
 import datetime
-import pytz
 import sys
 
 import gspread
@@ -22,11 +19,11 @@ import httplib2
 from time import sleep
 
 args = sys.argv
-jst = pytz.timezone('Asia/Tokyo')
 
 DATABASE_URL='postgresql://'+ args[1] + ':' + args[2] + '@'+ args[3] + ':5439/'+ args[4]
 SLACK_URL=args[5]
-DATE=int(args[6])
+KEYFILE_PATH=args[6]
+DATE=int(args[7])
 LOG='/tmp/superset.log'
 DEBUG=True
 CHECK_DATE=(datetime.date.today())-datetime.timedelta(days=DATE)
@@ -46,7 +43,7 @@ def get_dict_resultset(sql):
 
 # OAuth処理
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('gcp.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(KEYFILE_PATH, scope)
 gc = gspread.authorize(credentials)
 
 http = httplib2.Http(timeout=7200)
@@ -55,10 +52,11 @@ service = build('drive', 'v3', http=http)
 
 FILE_ID='1ucsOJTuUp6IFhz99MUVNtFZVeM1RjzkewOdok2X4VQg'
 
+PREV_APP_ID=""
+
 # Redshiftから収入取得
 apps_pa_revenue=get_dict_resultset("SELECT pa.name AS pa_name, a.id AS app_id, a.name AS app_name, platform, bundle_id, ad.name AS ad_name, Sum(revenue) AS revenue FROM (((daily_ad_revenue dar LEFT OUTER JOIN publisher_apps pa ON dar.publisher_app_id = pa.id) LEFT OUTER JOIN apps a ON pa.app_id = a.id) LEFT OUTER JOIN ad_networks ad ON pa.ad_network_id = ad.id) WHERE date = '{}' GROUP BY pa.ad_network_id, pa.name, a.id, a.name, store_id, platform, bundle_id, ad.name".format(str(CHECK_DATE)))
 
-PREV_APP_ID=""
 for pa_revenue in sorted(sorted(apps_pa_revenue, key=lambda x:x['app_id']), key=lambda x:x['bundle_id']):
 
     # 初回動作チェック
@@ -67,8 +65,11 @@ for pa_revenue in sorted(sorted(apps_pa_revenue, key=lambda x:x['app_id']), key=
     elif PREV_APP_ID == pa_revenue['app_id']:
         i = 1
     else:
+        sleep(1)
+        worksheet.update_cells(target_cells, value_input_option='USER_ENTERED')
         PREV_APP_ID = pa_revenue['app_id']
         i = 0
+
     PREV_APP_ID = pa_revenue['app_id']
 
     # 初期処理
@@ -82,12 +83,13 @@ for pa_revenue in sorted(sorted(apps_pa_revenue, key=lambda x:x['app_id']), key=
         if DEBUG:
             print(str(CHECK_DATE) + ": check_revenue_tt : " + SPREADSHEET_NAME)
     
-        # Googleスプレッドシート開く、無ければ作成
+        # Googleスプレッドシート無ければ作成
         try:
             gc = gspread.authorize(credentials)
             worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
+            print("ファイルオープン成功")
+#            sleep(1)
         except:
-            print("except が発生しました")
             new_file_body = {
                 'name': SPREADSHEET_NAME,  # 新しいファイルのファイル名. 省略も可能
                 'parents': ['1Z6nHs-LoO8D_HdXuY2wkH5yd2Uh70daP'],  # Copy先のFolder ID. 省略も可能
@@ -95,6 +97,9 @@ for pa_revenue in sorted(sorted(apps_pa_revenue, key=lambda x:x['app_id']), key=
                 'role': 'owner',
                 'emailAddress': 'ishizuka@tokyo-tsushin.com',
             }
+
+            print("ファイル作成")
+            print(FILE_ID)
             new_file = service.files().copy(
                 fileId=FILE_ID, body=new_file_body
             ).execute()
@@ -104,63 +109,93 @@ for pa_revenue in sorted(sorted(apps_pa_revenue, key=lambda x:x['app_id']), key=
         # 当日行取得、無ければ作る
         try:
             target = worksheet.find(str(CHECK_DATE))
-    
-        except:
+            sleep(1)
+
+            target_cells = worksheet.range(target.row, target.col - 1, target.row, target.col + 44)
+
+        # 無いので行を追加
+        except gspread.exceptions.CellNotFound as e:
+            if DEBUG:
+                print(type(e))
+                print("新規追加")
+                print(e)
+
+            sleep(2)
             # 売上集計シートに行追加
-            worksheet = gc.open(SPREADSHEET_NAME).worksheet("売上集計")
+            sales_summary = gc.open(SPREADSHEET_NAME).worksheet("売上集計")
             # リファレンス行をコピー
-            reference_list = worksheet.row_values(11, value_render_option='FORMULA')
+            reference_list = sales_summary.row_values(11, value_render_option='FORMULA')
             # 最終行にペースト
-            print(str(reference_list))
             del reference_list[0]
-            worksheet.append_row(reference_list, value_input_option='USER_ENTERED')
+            sales_summary.append_row(reference_list, value_input_option='USER_ENTERED')
 
-            # 集計シート開き直し
-            worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
+            # 書き込みデータ作成
+            target_list=['']*8
+            target_list[1]=str(CHECK_DATE)
+            target_list[2]=pa_revenue['app_name']
+            target_list[3]=0
+            target_list[4]=0
+            target_list[6]=0
+            target_list[7]=0
 
-            # 最終行に日付追加
-            worksheet.append_row(['', "{}".format(CHECK_DATE)], value_input_option='USER_ENTERED')
+            # 最終行に追加
+            worksheet.append_row(target_list, value_input_option='USER_ENTERED')
+
+            # 取得
             target = worksheet.find(str(CHECK_DATE))
+            target_cells = worksheet.range(target.row, target.col - 1, target.row, target.col + 44)
 
-        # リセット&数字を入れておく
-        try:
-            sleep(1)
-            worksheet.update_cell(target.row, 4, 0)
-            sleep(1)
-            worksheet.update_cell(target.row, 5, 0)
-            sleep(1)
-            worksheet.update_cell(target.row, 7, 0)
-            sleep(1)
-            worksheet.update_cell(target.row, 8, 0)
-        except:
-            sleep(5)
-            worksheet.update_cell(target.row, 8, 0)
-            sleep(2)
-            worksheet.update_cell(target.row, 7, 0)
-            sleep(2)
-            worksheet.update_cell(target.row, 5, 0)
-            sleep(2)
-            worksheet.update_cell(target.row, 4, 0)
+        except gspread.exceptions.APIError as e:
+            print(type(e))
+            print("API制限 スキップ")
+            continue
 
-    sleep(5)
+        except Exception as e:
+            print(type(e))
+
+        # 初期化
+        target_cells[3].value=0
+        target_cells[4].value=0
+        target_cells[6].value=0
+        target_cells[7].value=0
+#        try:
+#            sleep(1)
+#            worksheet.update_cell(target.row, 4, 0)
+#            sleep(1)
+#            worksheet.update_cell(target.row, 5, 0)
+#            sleep(1)
+#            worksheet.update_cell(target.row, 7, 0)
+#            sleep(1)
+#            worksheet.update_cell(target.row, 8, 0)
+#        except:
+#            sleep(5)
+#            worksheet.update_cell(target.row, 8, 0)
+#            sleep(2)
+#            worksheet.update_cell(target.row, 7, 0)
+#            sleep(2)
+#            worksheet.update_cell(target.row, 5, 0)
+#            sleep(2)
+#            worksheet.update_cell(target.row, 4, 0)
+
+#    sleep(5)
 
     # 接続確認
-    try:
-        worksheet.update_cell(target.row, 3, pa_revenue['app_name'])
-    except Exception as e:
-        print("APIError が発生しました")
-        # OAuth処理
-        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-        credentials = ServiceAccountCredentials.from_json_keyfile_name('gcp.json', scope)
-        gc = gspread.authorize(credentials)
-        
-        http = httplib2.Http(timeout=7200)
-        http = credentials.authorize(http)
-        service = build('drive', 'v3', http=http)
-        worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
-        worksheet.update_cell(target.row, 3, pa_revenue['app_name'])
-        print(type(e))
-        print(e)
+#    try:
+#        worksheet.update_cell(target.row, 3, pa_revenue['app_name'])
+#    except Exception as e:
+#        print("APIError が発生しました")
+#        # OAuth処理
+#        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+#        credentials = ServiceAccountCredentials.from_json_keyfile_name('gcp.json', scope)
+#        gc = gspread.authorize(credentials)
+#        
+#        http = httplib2.Http(timeout=7200)
+#        http = credentials.authorize(http)
+#        service = build('drive', 'v3', http=http)
+#        worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
+#        worksheet.update_cell(target.row, 3, pa_revenue['app_name'])
+#        print(type(e))
+#        print(e)
 
     # revenueをドルに戻す
     REVENUE=pa_revenue['revenue']/100
@@ -169,26 +204,30 @@ for pa_revenue in sorted(sorted(apps_pa_revenue, key=lambda x:x['app_id']), key=
     if 'Google AdMob' in pa_revenue['ad_name']:
         if DEBUG:
             print(pa_revenue['app_name'] + " : " + pa_revenue['platform'] + " : " + str(pa_revenue['bundle_id']) + " : " + pa_revenue['ad_name'] + " : 広告収入 $" + str(REVENUE))
-        COL_BANNER=4
-        COL_INTERSTITIAL=5
+        COL_BANNER=3
+        COL_INTERSTITIAL=4
         if 'バナー' in pa_revenue['pa_name'] or 'Banner' in pa_revenue['pa_name'] or 'banner' in pa_revenue['pa_name'] or 'レクタングル' in pa_revenue['pa_name'] or 'Rectangle' in pa_revenue['pa_name'] or 'rectangle' in pa_revenue['pa_name']:
-            worksheet.update_cell(target.row, COL_BANNER, float(worksheet.cell(target.row, COL_BANNER).value) + float(REVENUE))
+#            worksheet.update_cell(target.row, COL_BANNER, float(worksheet.cell(target.row, COL_BANNER).value) + float(REVENUE))
+            target_cells[COL_BANNER].value=float(target_cells[COL_BANNER].value or 0) + float(REVENUE)
 
         else:
-            worksheet.update_cell(target.row, COL_INTERSTITIAL, float(worksheet.cell(target.row, COL_INTERSTITIAL).value) + float(REVENUE))
+#            worksheet.update_cell(target.row, COL_INTERSTITIAL, float(worksheet.cell(target.row, COL_INTERSTITIAL).value) + float(REVENUE))
+            target_cells[COL_INTERSTITIAL].value=float(target_cells[COL_INTERSTITIAL].value or 0) + float(REVENUE)
 
     # Applovin出稿
     elif 'Applovin' in pa_revenue['ad_name']:
         if DEBUG:
             print(pa_revenue['app_name'] + " : " + pa_revenue['platform'] + " : " + str(pa_revenue['bundle_id']) + " : " + pa_revenue['ad_name'] + " : 広告収入 $" + str(REVENUE))
-        COL_BANNER=7
-        COL_INTERSTITIAL=8
+        COL_BANNER=6
+        COL_INTERSTITIAL=7
 #        if 'バナー' in pa_revenue['pa_name']:
         if 'バナー' in pa_revenue['pa_name'] or 'Banner' in pa_revenue['pa_name'] or 'banner' in pa_revenue['pa_name'] or 'レクタングル' in pa_revenue['pa_name'] or 'Rectangle' in pa_revenue['pa_name'] or 'rectangle' in pa_revenue['pa_name']:
-            worksheet.update_cell(target.row, COL_BANNER, float(worksheet.cell(target.row, COL_BANNER).value) + float(REVENUE))
+#            worksheet.update_cell(target.row, COL_BANNER, float(worksheet.cell(target.row, COL_BANNER).value) + float(REVENUE))
+            target_cells[COL_BANNER].value=float(target_cells[COL_BANNER].value or 0) + float(REVENUE)
 
         else:
-            worksheet.update_cell(target.row, COL_INTERSTITIAL, float(worksheet.cell(target.row, COL_INTERSTITIAL).value) + float(REVENUE))
+#            worksheet.update_cell(target.row, COL_INTERSTITIAL, float(worksheet.cell(target.row, COL_INTERSTITIAL).value) + float(REVENUE))
+            target_cells[COL_INTERSTITIAL].value=float(target_cells[COL_INTERSTITIAL].value or 0) + float(REVENUE)
 
 with open(LOG, mode='a') as f:
     f.write(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+": check_revenue_detail_tt end\n")
--- check_revenue_tt.py
+++ check_revenue_tt.py
@@ -2,13 +2,10 @@
 # -*- coding: utf8 -*-
 
 import requests
-import json
-from bs4 import BeautifulSoup
 import os
 import psycopg2
 import psycopg2.extras
 import datetime
-import pytz
 import sys
 
 import gspread
@@ -16,21 +13,19 @@ import json
 
 from oauth2client.service_account import ServiceAccountCredentials
 
-#from apiclient.discovery import build
 from googleapiclient.discovery import build
 import httplib2
 
 from time import sleep
 
 args = sys.argv
-jst = pytz.timezone('Asia/Tokyo')
 
 DATABASE_URL='postgresql://'+ args[1] + ':' + args[2] + '@'+ args[3] + ':5439/'+ args[4]
 SLACK_URL=args[5]
-DATE=int(args[6])
+KEYFILE_PATH=args[6]
+DATE=int(args[7])
 LOG='/tmp/superset.log'
 DEBUG=True
-#TODAY=datetime.date.today()
 CHECK_DATE=(datetime.date.today())-datetime.timedelta(days=DATE)
 
 with open(LOG, mode='a') as f:
@@ -48,7 +43,7 @@ def get_dict_resultset(sql):
 
 # OAuth処理
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('gcp.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(KEYFILE_PATH, scope)
 gc = gspread.authorize(credentials)
 
 http = httplib2.Http(timeout=7200)
@@ -69,6 +64,8 @@ for revenue in sorted(sorted(apps_revenue, key=lambda x:x['app_id']), key=lambda
     elif PREV_APP_ID == revenue['app_id']:
         i = 1
     else:
+        sleep(1)
+        worksheet.update_cells(target_cells, value_input_option='USER_ENTERED')
         PREV_APP_ID = revenue['app_id']
         i = 0
 
@@ -76,20 +73,21 @@ for revenue in sorted(sorted(apps_revenue, key=lambda x:x['app_id']), key=lambda
 
     # 初期処理
     if i == 0:
-
+        # スプレッドシート名生成
         if revenue['platform'] != 'android':
             SPREADSHEET_NAME="BOT_" + revenue['app_name'] + "／シミュレーション"
         else:
             SPREADSHEET_NAME="BOT_" + revenue['app_name'] + "_Android／シミュレーション"
-    
+
         if DEBUG:
             print(str(CHECK_DATE) + ": check_revenue_tt : " + SPREADSHEET_NAME)
-    
+
         # Googleスプレッドシート無ければ作成
         try:
             gc = gspread.authorize(credentials)
             worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
             print("ファイルオープン成功")
+#            sleep(1)
         except:
             new_file_body = {
                 'name': SPREADSHEET_NAME,  # 新しいファイルのファイル名. 省略も可能
@@ -98,55 +96,63 @@ for revenue in sorted(sorted(apps_revenue, key=lambda x:x['app_id']), key=lambda
                 'role': 'owner',
                 'emailAddress': 'ishizuka@tokyo-tsushin.com',
             }
-        
+
             print("ファイル作成")
             print(FILE_ID)
             new_file = service.files().copy(
                 fileId=FILE_ID, body=new_file_body
             ).execute()
-    
+
             gc = gspread.authorize(credentials)
             worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
-    
+
         # 当日行取得、無ければ作る
         try:
             target = worksheet.find(str(CHECK_DATE))
-    
-        except:
-            # 売上集計シートに行追加
-            worksheet = gc.open(SPREADSHEET_NAME).worksheet("売上集計")
-            # リファレンス行をコピー
-            reference_list = worksheet.row_values(11, value_render_option='FORMULA')
-            # 最終行にペースト
-            print(str(reference_list))
-            del reference_list[0]
-            worksheet.append_row(reference_list, value_input_option='USER_ENTERED')
+            sleep(1)
+            target_cells = worksheet.range(target.row, target.col - 1, target.row, target.col + 44)
 
-            # 集計シート開き直し
-            worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
+        # 無いので行を追加
+        except gspread.exceptions.CellNotFound as e:
+            if DEBUG:
+                print(type(e))
+                print("新規追加")
+                print(e)
+
+            # 売上集計シートに行追加
+            sleep(1)
+            sales_summary = gc.open(SPREADSHEET_NAME).worksheet("売上集計")
+            # リファレンス行をコピー
+            sleep(1)
+            reference_list = sales_summary.row_values(11, value_render_option='FORMULA')
+            # 最終行にペースト
+            del reference_list[0]
+            sleep(1)
+            sales_summary.append_row(reference_list, value_input_option='USER_ENTERED')
+
+            # 書き込みデータ作成
+            target_list=['']*3
+            target_list[1]=str(CHECK_DATE)
+            target_list[2]=revenue['app_name']
+
+            # 最終行に追加
+            sleep(1)
+            worksheet.append_row(target_list, value_input_option='USER_ENTERED')
 
             # 最終行に日付追加
-            target = worksheet.append_row(['', "{}".format(CHECK_DATE)], value_input_option='USER_ENTERED')
+            sleep(1)
             target = worksheet.find(str(CHECK_DATE))
+            sleep(1)
+            target_cells = worksheet.range(target.row, target.col - 1, target.row, target.col + 44)
 
-    sleep(4)
-    # アプリ名記入して接続確認
-    try:
-        worksheet.update_cell(target.row, 3, revenue['app_name'])
-    except Exception as e:
-        print("APIError が発生しました")
-        # OAuth処理
-        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-        credentials = ServiceAccountCredentials.from_json_keyfile_name('gcp.json', scope)
-        gc = gspread.authorize(credentials)
-        
-        http = httplib2.Http(timeout=7200)
-        http = credentials.authorize(http)
-        service = build('drive', 'v3', http=http)
-        worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
-        worksheet.update_cell(target.row, 3, revenue['app_name'])
-        print(type(e))
-        print(e)
+        except gspread.exceptions.APIError as e:
+            print(type(e))
+            print("API制限 スキップ")
+            continue
+
+        except Exception as e:
+            print(type(e))
+            print("新規エラー")
 
     # revenueをドルに戻す
     REVENUE=revenue['revenue']/100
@@ -155,7 +161,7 @@ for revenue in sorted(sorted(apps_revenue, key=lambda x:x['app_id']), key=lambda
 
     # Unity Ads出稿
     if revenue['ad_name'] == 'Unity Ads':
-        worksheet.update_cell(target.row, 6, REVENUE)
+        target_cells[5].value=REVENUE
 
     # nend出稿
 #    if revenue['ad_name'] == '':
@@ -163,39 +169,51 @@ for revenue in sorted(sorted(apps_revenue, key=lambda x:x['app_id']), key=lambda
 
     # Ad Generation出稿
     elif revenue['ad_name'] == 'Ad Generation':
-        worksheet.update_cell(target.row, 10, REVENUE)
+        target_cells[9].value=REVENUE
 
     # FIVE出稿
     elif revenue['ad_name'] == 'FIVE':
-        worksheet.update_cell(target.row, 11, REVENUE)
+        target_cells[10].value=REVENUE
 
     # i-mobile Affiliate出稿
     elif revenue['ad_name'] == 'i-mobile Affiliate':
-        worksheet.update_cell(target.row, 13, REVENUE)
+        target_cells[12].value=REVENUE
 
     # ironSource-Publisher出稿
     elif revenue['ad_name'] == 'ironSource-Publisher':
-        worksheet.update_cell(target.row, 15, REVENUE)
+        target_cells[14].value=REVENUE
 
     # Tapjoy出稿
     elif revenue['ad_name'] == 'Tapjoy':
-        worksheet.update_cell(target.row, 16, REVENUE)
+        target_cells[15].value=REVENUE
 
     # Facebook Audience Network出稿
     elif revenue['ad_name'] == 'Facebook Audience Network':
-        worksheet.update_cell(target.row, 17, REVENUE)
+        target_cells[16].value=REVENUE
 
     # Vungle出稿
     elif revenue['ad_name'] == 'Vungle':
-        worksheet.update_cell(target.row, 30, REVENUE)
+        target_cells[29].value=REVENUE
 
     # TikTok Audience Network出稿
     elif revenue['ad_name'] == 'TikTok Audience Network':
-        worksheet.update_cell(target.row, 35, REVENUE)
+        target_cells[34].value=REVENUE
 
     # Mintegral Publisher出稿
     elif revenue['ad_name'] == 'Mintegral Publisher':
-        worksheet.update_cell(target.row, 36, REVENUE)
+        target_cells[35].value=REVENUE
+
+    # Google AdMob出稿
+    elif revenue['ad_name'] == 'Google AdMob':
+        pass
+
+    # Applovin出稿
+    elif revenue['ad_name'] == 'Applovin':
+        pass
+
+    else:
+        print("DEBUG DEBUG DEBUG!")
+        print(str(revenue['ad_name']) + " : " + str(REVENUE))
 
 with open(LOG, mode='a') as f:
     f.write(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+": check_revenue_tt end\n")
