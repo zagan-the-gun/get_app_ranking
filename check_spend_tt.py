c807f3acb94d4c73d710405ab48d6c7ccfacd99f
zagan <zagan.the.gun@gmail.com>
2019-11-18 14:34:16 +0900

    仮でこっちにしてみる

--- check_spend_tt.py
+++ check_spend_tt.py
@@ -25,7 +25,7 @@ SLACK_URL=args[5]
 KEYFILE_PATH=args[6]
 DATE=int(args[7])
 LOG='/tmp/superset.log'
-DEBUG=False
+DEBUG=True
 CHECK_DATE=(datetime.date.today())-datetime.timedelta(days=DATE)
 
 with open(LOG, mode='a') as f:
@@ -52,10 +52,35 @@ service = build('drive', 'v3', http=http)
 
 FILE_ID='1ogWdNTKbLVfGGD7-EMrBWfbwXzu_swn7fqmQk2X-2Us'
 
+print(str(CHECK_DATE))
 PREV_APP_ID=""
 # Redshiftから支出取得
-apps_spend=get_dict_resultset("SELECT a.id AS app_id, a.name AS app_name, store_id, platform, bundle_id, ad.id AS ad_id, ad.name AS ad_name, ds.date, Sum(spend) AS spend, Sum(installs) AS installs, Sum(clicks) AS clicks, Sum(impressions) AS impressions, original_currency, Sum(original_spend) AS original_spend FROM (((daily_spend ds LEFT OUTER JOIN campaigns c ON ds.campaign_id = c.id) LEFT OUTER JOIN apps a ON c.app_id = a.id) LEFT OUTER JOIN ad_networks ad ON c.ad_network_id = ad.id) WHERE date = '{}' GROUP BY c.ad_network_id, a.id, a.name, store_id, platform, bundle_id, ad.id, ad.name, ds.date, original_currency".format(str(CHECK_DATE)))
-for spend in sorted(sorted(apps_spend, key=lambda x:x['app_id']), key=lambda x:x['bundle_id']):
+#apps_spend=get_dict_resultset("SELECT a.id AS app_id, a.name AS app_name, store_id, platform, bundle_id, ad.id AS ad_id, ad.name AS ad_name, ds.date, Sum(spend) AS spend, Sum(installs) AS installs, Sum(clicks) AS clicks, Sum(impressions) AS impressions, original_currency, Sum(original_spend) AS original_spend FROM (((daily_spend ds LEFT OUTER JOIN campaigns c ON ds.campaign_id = c.id) LEFT OUTER JOIN apps a ON c.app_id = a.id) LEFT OUTER JOIN ad_networks ad ON c.ad_network_id = ad.id) WHERE date = '{}' GROUP BY c.ad_network_id, a.id, a.name, store_id, platform, bundle_id, ad.id, ad.name, ds.date, original_currency".format(str(CHECK_DATE)))
+
+"""
+apps_spend=get_dict_resultset("\
+        SELECT a.id AS app_id, a.name AS app_name, store_id, a.platform AS platform, bundle_id, ad.id AS ad_id, ad.name AS ad_name, ds.date, Sum(spend) AS spend, Sum(installs) AS installs, Sum(clicks) AS clicks, Sum(impressions) AS impressions, original_currency, Sum(original_spend) AS original_spend \
+        FROM (((daily_spend ds \
+        LEFT OUTER JOIN campaigns c ON ds.campaign_id = c.id) \
+        LEFT OUTER JOIN apps a ON c.app_id = a.id) \
+        LEFT OUTER JOIN ad_networks ad ON c.ad_network_id = ad.id) \
+        WHERE ds.date = '{}' \
+        GROUP BY c.ad_network_id, a.id, a.name, store_id, a.platform, bundle_id, ad.id, ad.name, ds.date, original_currency".format(str(CHECK_DATE)))
+"""
+
+apps_spend=get_dict_resultset("\
+        SELECT a.id AS app_id, a.name AS app_name, store_id, a.platform AS platform, bundle_id, ad.id AS ad_id, ad.name AS ad_name, rm.date, Sum(reported_spend) AS spend, Sum(tracked_installs) AS installs \
+        FROM (((reporting_metrics rm \
+        LEFT OUTER JOIN campaigns c ON rm.campaign_id = c.id) \
+        LEFT OUTER JOIN apps a ON c.app_id = a.id) \
+        LEFT OUTER JOIN ad_networks ad ON c.ad_network_id = ad.id) \
+        WHERE rm.date = '{}' \
+        GROUP BY c.ad_network_id, a.id, a.name, store_id, a.platform, bundle_id, ad.id, ad.name, rm.date".format(str(CHECK_DATE)))
+
+for spend in sorted(sorted(apps_spend, key=lambda x:x['app_id'] or ""), key=lambda x:x['bundle_id'] or ""):
+
+    if (spend['app_id'] is None) or (spend['ad_id'] is None):
+        continue
 
     # 初回動作チェック
     if PREV_APP_ID == "":
@@ -63,10 +88,16 @@ for spend in sorted(sorted(apps_spend, key=lambda x:x['app_id']), key=lambda x:x
     elif PREV_APP_ID == spend['app_id']:
         i = 1
     else:
-        sleep(2)
-        worksheet.update_cells(target_cells, value_input_option='USER_ENTERED')
-        PREV_APP_ID = spend['app_id']
         i = 0
+        PREV_APP_ID = spend['app_id']
+
+        try:
+            sleep(2)
+            worksheet.update_cells(target_cells, value_input_option='USER_ENTERED')
+
+        except gspread.exceptions.APIError as e:
+            with open(LOG, mode='a') as f:
+                f.write(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+": check_spend_tt : API制限 データ書き込み失敗 スキップ : " + str(CHECK_DATE) + " : " + SPREADSHEET_NAME + "\n")
 
     PREV_APP_ID = spend['app_id']
 
@@ -83,8 +114,6 @@ for spend in sorted(sorted(apps_spend, key=lambda x:x['app_id']), key=lambda x:x
 
         # Googleスプレッドシート無ければ作成
         try:
-            # gc行は要らないかも
-#            gc = gspread.authorize(credentials)
             sleep(2)
             worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
             if DEBUG:
@@ -92,10 +121,12 @@ for spend in sorted(sorted(apps_spend, key=lambda x:x['app_id']), key=lambda x:x
 
         except gspread.exceptions.APIError as e:
             with open(LOG, mode='a') as f:
-                f.write(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+": check_spend_tt : API制限 ファイルオープン失敗 再試行 " + SPREADSHEET_NAME + "\n")
+                f.write(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+": check_spend_tt : API制限 ファイルオープン失敗 スキップ : " + str(CHECK_DATE) + " : " + SPREADSHEET_NAME + "\n")
 
-            sleep(3)
-            worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
+            if DEBUG:
+                print(type(e))
+                print("API制限 ファイルオープン失敗 スキップ " + SPREADSHEET_NAME)
+            continue
 
         except:
             with open(LOG, mode='a') as f:
@@ -104,19 +135,18 @@ for spend in sorted(sorted(apps_spend, key=lambda x:x['app_id']), key=lambda x:x
             new_file_body = {
                 'name': SPREADSHEET_NAME,  # 新しいファイルのファイル名. 省略も可能
                 'parents': ['1Z6nHs-LoO8D_HdXuY2wkH5yd2Uh70daP'],  # Copy先のFolder ID. 省略も可能
-                'type': 'user',
-                'role': 'owner',
-                'emailAddress': 'ishizuka@tokyo-tsushin.com',
             }
 
             if DEBUG:
                 print("ファイル作成")
                 print(FILE_ID)
+            sleep(2)
             new_file = service.files().copy(
                 fileId=FILE_ID, body=new_file_body
             ).execute()
 
             gc = gspread.authorize(credentials)
+            sleep(2)
             worksheet = gc.open(SPREADSHEET_NAME).worksheet("集計シート")
     
         # 当日行取得、無ければ作る
@@ -135,11 +165,14 @@ for spend in sorted(sorted(apps_spend, key=lambda x:x['app_id']), key=lambda x:x
                 print(e)
 
             # 売上集計シートに行追加
+            sleep(2)
             sales_summary = gc.open(SPREADSHEET_NAME).worksheet("売上集計")
             # リファレンス行をコピー
+            sleep(2)
             reference_list = sales_summary.row_values(11, value_render_option='FORMULA')
             # 最終行にペースト
             del reference_list[0]
+            sleep(2)
             sales_summary.append_row(reference_list, value_input_option='USER_ENTERED')
 
             # 書き込みデータ作成
@@ -148,25 +181,33 @@ for spend in sorted(sorted(apps_spend, key=lambda x:x['app_id']), key=lambda x:x
             target_list[2]=spend['app_name']
 
             # 最終行に追加
+            sleep(2)
             worksheet.append_row(target_list, value_input_option='USER_ENTERED')
 
             # 最終行に日付追加
+            sleep(2)
             target = worksheet.find(str(CHECK_DATE))
 
+            sleep(2)
             target_cells = worksheet.range(target.row, target.col - 1, target.row, target.col + 44)
 
         except gspread.exceptions.APIError as e:
-            print(type(e))
-            print("API制限 スキップ")
+            with open(LOG, mode='a') as f:
+                f.write(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+": check_spend_tt : API制限 当日行処理失敗 スキップ : " + str(CHECK_DATE) + " : " + "\n")
+
+            if DEBUG:
+                print(type(e))
+                print("API制限 当日行処理失敗 スキップ " + SPREADSHEET_NAME)
             continue
 
         except Exception as e:
             print(type(e))
 
     # spendをドルに戻す
-    SPEND=spend['spend']/100
+#    SPEND=spend['spend']/100
+    SPEND=round((spend['spend'] or 0)/100, 2)
     if DEBUG:
-        print(spend['app_name'] + " : " + spend['platform'] + " : " + str(spend['store_id']) + " : " + str(spend['bundle_id']) + " : " + spend['ad_name'] + " : インストール " + str(spend['installs']) + " : 広告支出 $" + str(SPEND))
+        print(spend['app_name'] + " : " + spend['platform'] + " : " + str(spend['store_id']) + " : " + str(spend['bundle_id']) + " : " + spend['ad_name'] + " : インストール " + str(spend['installs'] or 0) + " : 広告支出 $" + str(SPEND))
 
     # Applovin出稿
     if spend['ad_name'] == 'Applovin':
